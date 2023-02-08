56fe4f5faf340767a1b771a182f1f7a5e4bd4730
Salvatore Scalisi <scalisi.salvo02@gmail.com>
2023-02-08 16:30:53 +0100

    create students table and insert random values and print values

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,13 +16,22 @@ public class Main {
                     " first_name VARCHAR(30) NOT NULL, " +
                     " PRIMARY KEY (student_id))";
             statement.executeUpdate(sql);
-            String[] lastNames = {"Rossi", "La manna", "Gatto", "Sassari"};
-            String[] firstNames = {"Mario", "Giovanni", "Flavia", "Giulia"};
-            for (int i = 0; i < 4; i++) {
-                    sql = "SELECT * FROM students WHERE last_name='" + lastNames[i] + "' AND first_name='" + firstNames[i] + "'";
+
+            List<String> lastNames = new ArrayList<>();
+            lastNames.add("Rossi");
+            lastNames.add("La manna");
+            lastNames.add("Gatto");
+            lastNames.add("Sassari");
+            List<String> firstNames = new ArrayList<>();
+            firstNames.add("Mario");
+            firstNames.add("Giovanni");
+            firstNames.add("Flavia");
+            firstNames.add("Giulia");
+            for (int i=0; i < firstNames.size(); i++) {
+                    sql = "SELECT * FROM students WHERE last_name='" + lastNames.get(i) + "' AND first_name='" + firstNames.get(i) + "'";
                     resultSet=statement.executeQuery(sql);
                     if(!resultSet.next()){
-                        sql = "INSERT INTO students (last_name, first_name) VALUES('" + lastNames[i] + "','" + firstNames[i] + "')";
+                        sql = "INSERT INTO students (last_name, first_name) VALUES('" + lastNames.get(i) + "','" + firstNames.get(i) + "')";
                         statement.executeUpdate(sql);
                     }
                 }
